59fb7ab52794a3740f8ee0ff96773f5ae7d0c62d
Benjamin <RichMan@MacBook-Pro-RichMan.local>
2024-07-13 16:28:58 +0500

    java script

---
+++ main.js
@@ -0,0 +1,95 @@
+const what1 = prompt("what is your name?")
+console.log(what1);
+alert('your answer  ' + what1)
+
+const what2 = prompt("what is your surname?")
+console.log(what2);
+alert('your answer  ' + what2)
+
+const what3 = prompt("what color do you like?")
+console.log(what3);
+alert('your answer  ' + what3)
+
+const what4 = prompt("what do you like to do?")
+console.log(what4);
+alert('your answer  ' + what4)
+
+const what5 = prompt("what is your fathers name?")
+console.log(what5);
+alert('your answer  ' + what5)
+
+const what6 = prompt("what is your mothers name?")
+console.log(what6);
+alert('your answer  ' + what6)
+
+const what7 = prompt("what is your brothers/sisters name?")
+console.log(what7);
+alert('your answer  ' + what7)
+
+const what8 = prompt("do you have any wild animals?")
+console.log(what8);
+alert('your answer  ' + what8)
+
+const what9 = prompt("do you have girlfriend?")
+console.log(what9);
+alert('your answer  ' + what9)
+
+const what10 = prompt("do you have friends?")
+console.log(what10);
+alert('your answer  ' + what10)
+
+const what11 = prompt("do you have computer?")
+console.log(what11);
+alert('your answer  ' + what11)
+
+const what12 = prompt("do you play computer?")
+console.log(what12);
+alert('your answer  ' + what12)
+
+const what13 = prompt("do you have like computer games?")
+console.log(what13);
+alert('your answer  ' + what13)
+
+const what14 = prompt("do you agree with me?")
+console.log(what14);
+alert('your answer  ' + what14)
+
+const what15 = prompt("do you know about mars it?")
+console.log(what15);
+alert('your answer  ' + what15)
+
+const what16 = prompt("do you know about cambrdge?")
+console.log(what16);
+alert('your answer  ' + what16)
+
+const what17 = prompt("do you know about international?")
+console.log(what17);
+alert('your answer  ' + what18)
+
+const what19 = prompt("do you like english?")
+console.log(what19);
+alert('your answer  ' + what19)
+
+const what20 = prompt("do you like russian?")
+console.log(what20);
+alert('your answer  ' + what20)
+
+const what21 = prompt("do you like uzbekish?")
+console.log(what21);
+alert('your answer  ' + what21)
+
+const what22 = prompt("do you know english?")
+console.log(what22);
+alert('your answer  ' + what22)
+
+const what23 = prompt("do you know russian?")
+console.log(what23);
+alert('your answer  ' + what23)
+
+const what24 = prompt("do you know uzbekish?")
+console.log(what24);
+alert('your answer  ' + what24)
+
+const what25 = prompt("did you finished?")
+console.log(what25);
+alert('your answer  ' + what25)
